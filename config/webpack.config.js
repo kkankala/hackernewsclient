3315700dbecad9ba08c85ae144710d82cda16d9a
Kalyan Kankala <kankala.kalyan@gmail.com>
2019-09-25 07:57:36 -0400

    testing htmlplugin n chunk

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -257,17 +257,17 @@ module.exports = function(webpackEnv) {
               : false
           }
         })
-      ],
-      // Automatically split vendor and commons
-      // https://twitter.com/wSokra/status/969633336732905474
-      // https://medium.com/webpack/webpack-4-code-splitting-chunk-graph-and-the-splitchunks-optimization-be739a861366
-      splitChunks: {
-        chunks: 'all',
-        name: false
-      },
-      // Keep the runtime chunk separated to enable long term caching
-      // https://twitter.com/wSokra/status/969679223278505985
-      runtimeChunk: true
+      ]
+      // // Automatically split vendor and commons
+      // // https://twitter.com/wSokra/status/969633336732905474
+      // // https://medium.com/webpack/webpack-4-code-splitting-chunk-graph-and-the-splitchunks-optimization-be739a861366
+      // splitChunks: {
+      //   chunks: 'all',
+      //   name: false
+      // },
+      // // Keep the runtime chunk separated to enable long term caching
+      // // https://twitter.com/wSokra/status/969679223278505985
+      // runtimeChunk: true
     },
     resolve: {
       // This allows you to set a fallback for where Webpack should look for modules.
